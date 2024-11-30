9505a4440ee5c8a7dfe3ccd0e8f35641a2e73753
Sumaiya Begum <sumaiyabegum43@gmail.com>
2024-11-29 18:37:04 -0800

    navbar added

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import Link from "next/link";
 import Image from "next/image";
 import Menu from "@/components/Menu";
+import Navbar from "@/components/Navbar";
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -20,7 +21,7 @@ export default function DashboardLayout({
   return (
     <div className="h-screen flex">
       {/* LEFT */}
-      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] ">
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%]">
         <Link href="" className="flex items-center justify-center lg:justify-start gap-2 p-4">
           <Image src="/logo.png" alt="logo" width={32} height={32}/>
           <span className="hidden lg:block">MazSchool</span>
@@ -29,8 +30,9 @@ export default function DashboardLayout({
       </div>
 
       {/* RIGHT */}
-      <div className="w-[86%] md:w-[92%] lg:w-[82%] xl:w-[86%] ">
-        R
+      <div className="w-[86%] md:w-[92%] lg:w-[82%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll ">
+        <Navbar/>
+        {children}
       </div>
     </div>
   );
